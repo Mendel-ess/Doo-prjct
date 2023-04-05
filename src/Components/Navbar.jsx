d8f0335249ae1b253ce78e384bc577e859997500
Jhonnatan Mendez <jhonnatandavid34mc@gmail.com>
2023-04-05 14:53:26 -0500

    volvio a funcionar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,9 +4,6 @@ export default function Navbar() {
         <nav>
             <ul>
                 <li><Link to="/">Homepage</Link></li>
-                <li><Link to="/peliculas">Peliculas</Link></li>
-                <li><Link to="/series">Series</Link></li>
-                <li><Link to="/login">Iniciar sesion</Link></li>
                 <li><Link to="/registrar">Registrar</Link></li>
             </ul>
         </nav>
